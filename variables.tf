e130607ab667e0f657b306aa226b9ad03ac8423c
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-19 09:34:18 +0100

    feat(validation):add more validation that ensures subnet address prefixes are within the vnet address space and nsg rules have unique priorities

--- variables.tf
+++ variables.tf
@@ -143,4 +143,56 @@ variable "config" {
     ])
     error_message = "One or more subnets reference a shared route_table that does not exist in route_tables."
   }
+
+  validation {
+    condition = alltrue(flatten([
+      for subnet in values(var.config.subnets) : [
+        for prefix in subnet.address_prefixes :
+        can(cidrhost(prefix, 0)) &&
+        anytrue([
+          for vnet_space in var.config.address_space :
+          can(cidrhost(vnet_space, 0)) &&
+          (
+            (
+              tonumber(element(split(".", cidrhost(prefix, 0)), 0)) * 256 * 256 * 256 +
+              tonumber(element(split(".", cidrhost(prefix, 0)), 1)) * 256 * 256 +
+              tonumber(element(split(".", cidrhost(prefix, 0)), 2)) * 256 +
+              tonumber(element(split(".", cidrhost(prefix, 0)), 3))
+              ) >= (
+              tonumber(element(split(".", cidrhost(vnet_space, 0)), 0)) * 256 * 256 * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, 0)), 1)) * 256 * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, 0)), 2)) * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, 0)), 3))
+            )
+          ) &&
+          (
+            (
+              tonumber(element(split(".", cidrhost(prefix, -1)), 0)) * 256 * 256 * 256 +
+              tonumber(element(split(".", cidrhost(prefix, -1)), 1)) * 256 * 256 +
+              tonumber(element(split(".", cidrhost(prefix, -1)), 2)) * 256 +
+              tonumber(element(split(".", cidrhost(prefix, -1)), 3))
+              ) <= (
+              tonumber(element(split(".", cidrhost(vnet_space, -1)), 0)) * 256 * 256 * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, -1)), 1)) * 256 * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, -1)), 2)) * 256 +
+              tonumber(element(split(".", cidrhost(vnet_space, -1)), 3))
+            )
+          )
+        ])
+      ]
+    ]))
+    error_message = "All subnet address prefixes must be within the VNet address space."
+  }
+
+  validation {
+    condition = alltrue([
+      for nsg in concat(
+        [for subnet in values(var.config.subnets) : lookup(subnet, "network_security_group", null)],
+        values(var.config.network_security_groups)
+        ) : nsg != null ? (
+        length(distinct([for rule in values(nsg.rules) : rule.priority])) == length(nsg.rules)
+      ) : true
+    ])
+    error_message = "Each NSG rule must have a unique priority within its NSG."
+  }
 }
